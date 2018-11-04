a224461f9dde326d05682ec62f73954dd405e123
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-03 23:25:31 -0500

    Update main.lua

--- expb/modules/main.lua
+++ expb/modules/main.lua
@@ -2,6 +2,10 @@ local discordia = require('discordia')
 local client = discordia.Client()
 local module = {}
 
-
+function module.chat(message)
+  if message.content == "expb.test" then
+    message.channel:send("Bot Functional")
+    end
+  end
 
 return module
