c6279ce7020f774ebb71144c0a219b240734bf5b
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-03 23:21:45 -0500

    Update expbot.lua

--- expbot.lua
+++ expbot.lua
@@ -2,4 +2,15 @@ local discordia = require('discordia')
 local client = discordia.Client()
 token = require("expotoken")
 
+local mods = require("expb/mods.lua")
+
+
+
+client:on("messageCreate", function(message)
+    
+    if message.guild then if message.channel.nsfw then
+        end end
+    
+    end)
+
 client:run(token)
