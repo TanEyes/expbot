bd274f72aaf09ba8eebcaacf00f3624da9dc8508
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-06 20:40:20 -0600

    Update weightgain.lua

--- expb/modules/weightgain.lua
+++ expb/modules/weightgain.lua
@@ -5,8 +5,8 @@ function module.init(mods)
     end
 function module.chat(m)
     local cur = vars.mods.userdata.getCurrency(m.member)
-    vars.mods.userdata.nUpdate(m.member, "weight", 1, cur.weight[1] ^ 10)
-    print("power 10")
+    vars.mods.userdata.nUpdate(m.member, "weight", 1, cur.weight[1] ^ 2)
+    print("power 2")
     if m.content == "expb.weigh" then
         m:reply(vars.mods.userdata.getCurrency(m.member).weight[1])
         end
