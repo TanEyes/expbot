c60ad91303d275aa9151310794749c17a126162e
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-06 21:05:18 -0600

    Update main.lua

--- expb/modules/main.lua
+++ expb/modules/main.lua
@@ -2,6 +2,7 @@ local module = {}
 
 function module.chat(message)
   if message.content == "expb.update" and message.author.id == "115529446370443265" then
+    os.execute('cd "C:\Users\mrfee\Luvit"')
 os.execute("git pull https://github.com/TanEyes/expbot --allow-unrelated-histories")
 print("Updated.")
 io.close()
