0f896639d502b515f9ac37c816b77315f30143bf
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-06 20:46:57 -0600

    Update weightgain.lua

--- expb/modules/weightgain.lua
+++ expb/modules/weightgain.lua
@@ -5,12 +5,12 @@ function module.init(mods)
     end
 function module.chat(m)
     local cur = vars.mods.userdata.getCurrency(m.member)
-    vars.mods.userdata.nUpdate(m.member, "weight", 1, cur.weight[1] + 1000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+    vars.mods.userdata.nUpdate(m.member, "weight", 1, cur.weight[1] + 100000000000000000000000000000000000000000000000000000000000)
     print("big")
     if m.content == "expb.weigh" then
         weigh = vars.mods.userdata.getCurrency(m.member)
-        if weigh.weight[#weigh.weight-1] then str = weigh.weight[#weigh.weight-1]
-            while string.len(str) < string.len("1000000000000000000000000000000000000000000000000000000000000000000000000000000000") do
+        if weigh.weight[#weigh.weight-1] then str = string.format("%.0f", weigh.weight[#weigh.weight-1])
+            while string.len(str) < string.len("100000000000000000000000000000000000000000000000000000000000") do
                 str = "0" .. str end str = "." ..str 
         else str = "" end
         m:reply(weigh.weight[#weigh.weight] .. str)
