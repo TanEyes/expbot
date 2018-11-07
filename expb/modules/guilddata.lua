670992a158d5df351d81645eaa6bb36b39870a4b
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-06 20:14:50 -0600

    Create guilddata.lua

---
+++ expb/modules/guilddata.lua
@@ -0,0 +1,90 @@
+local module = {}
+
+currency = {}
+function loads(mem)
+if currency[mem.id] == nil then
+	survive = false
+	aa = io.open("expb/userdata/".. mem.id ..".lua" ,"r")
+	if aa then
+	for line in io.lines("expb/userdata/".. mem.id ..".lua") do if string.find(line, "return ret") then survive = true end end
+end
+	if aa and survive then
+		currency[mem.id] = require("expb/userdata/".. mem.id)
+		aa:close()
+	else
+		currency[mem.id] = {
+				weight = {0}, eatCount = {1}, prestige = {0}
+		}
+	end
+end
+end
+function save(guild)
+	if type(currency[guild.id]) == "table" then
+aa = io.open("expb/userdata/".. guild.id ..".lua", "w")
+io.output(aa)
+ee= ""
+for i, k in pairs(currency[guild.id]) do
+	if type(k) == "table" then str = ""
+				for c, v in pairs(k) do if c < #k then str = str .. v .."," else str = str .. v end end
+		ee = ee .."ret.".. i .." = {".. str .."}\n"
+		else
+		ee = ee .."ret.".. i .." = ".. k .."\n"
+		end
+end
+io.write("ret = {} ".. ee .."\nreturn ret")
+io.close()
+end
+	end
+function curfind(guild) 
+  load = false
+  for i, k in pairs(currency) do
+    if i == guild.id then load = true end
+    end
+  if not load then loads(guild) end
+	return currency[guild.id]
+  end
+function module.chat(message)
+	curfind(message.author)
+	for i=1, #currency[message.author.id].weight do
+		if currency[message.author.id].weight[i] >= 1000000000000000000000000000000 then
+			if currency[message.author.id].weight[i+1] then
+				currency[message.author.id].weight[i+1] = currency[message.author.id].weight[i+1] + 1
+				currency[message.author.id].weight[i] = currency[message.author.id].weight[i] - 1000000000000000000000000000000
+				else currency[message.author.id].weight[i+1] = 1
+				currency[message.author.id].weight[i] = currency[message.author.id].weight[i] - 1000000000000000000000000000000
+				end
+			end
+		end
+	for i=1, #currency[message.author.id].eatCount do
+		if currency[message.author.id].eatCount[i] >= 1000000000000000000000000000000 then
+			if currency[message.author.id].eatCount[i+1] then
+				currency[message.author.id].eatCount[i+1] = currency[message.author.id].eatCount[i+1] + 1
+				currency[message.author.id].eatCount[i] = currency[message.author.id].eatCount[i] - 1000000000000000000000000000000
+				else currency[message.author.id].prestige[i+1] = 1
+				currency[message.author.id].eatCount[i] = currency[message.author.id].eatCount[i] - 1000000000000000000000000000000
+				end
+			end
+		end
+	for i=1, #currency[message.author.id].prestige do
+		if currency[message.author.id].prestige[i] >= 1000000000000000000000000000000 then
+			if currency[message.author.id].prestige[i+1] then
+				currency[message.author.id].prestige[i+1] = currency[message.author.id].prestige[i+1] + 1
+				currency[message.author.id].prestige[i] = currency[message.author.id].prestige[i] - 1000000000000000000000000000000
+				else currency[message.author.id].prestige[i+1] = 1
+				currency[message.author.id].prestige[i] = currency[message.author.id].prestige[i] - 1000000000000000000000000000000
+				end
+			end
+		end
+	save(message.author)
+  end
+
+function module.getCurrency(guild)
+	return curfind(guild)
+	end
+
+function module.nUpdate(guild, table, id, value)
+	currency[guild.id][table][id] = value
+	save(guild)
+	end
+
+return module
